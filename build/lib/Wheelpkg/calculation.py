c791096d790ba3b76a16837c4e100e55ff7157d2
Vikram Reddy <vikram.reddy@ibm.com>
2023-02-20 12:02:05 +0530

    testing_ml

---
+++ build/lib/Wheelpkg/calculation.py
@@ -0,0 +1,9 @@
+def addition(a,b):
+    return a+b
+
+def substraction(a,b):
+    return a-b
+
+def multilplication(a,b):
+    return a*b    
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='Wheelpkg',
+    version='0.0.1',
+    author='vikram',
+    author_email='dsvr09@gmail.com',
+    description='calculation',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python ::3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7' ,
+)
